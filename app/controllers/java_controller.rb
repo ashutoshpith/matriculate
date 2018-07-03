0c6f63072d16aea49e19ecd34d9638d8455e9729
ashutosh <amxdp18@gmail.com>
2018-07-03 17:42:13 +0530

    remove controller java page

--- app/controllers/java_controller.rb
+++ app/controllers/java_controller.rb
@@ -2,12 +2,7 @@ class JavaController < ApplicationController
   def read
   end
 
-  def practice
-  end
+ 
 
-  def challange
-  end
-
-  def progress
-  end
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,7 @@ Rails.application.routes.draw do
   
   get 'javacat/start' 
   get 'java/read'
-  get 'java/practice'
-  get 'java/challenge'
-  get 'java/progress'
+ 
   get 'welcome/home'
   root 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
